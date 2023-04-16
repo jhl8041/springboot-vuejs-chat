e43d33fe25f4e0721903b51b0138682d93340ba9
jaehyunglee <jhl8041@gmail.com>
2023-04-16 17:54:29 +0900

    initial commit

---
+++ src/main/java/com/dannyjae/springbootvuejschat/SpringbootVuejsChatApplication.java
@@ -0,0 +1,13 @@
+package com.dannyjae.springbootvuejschat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootVuejsChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootVuejsChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dannyjae/springbootvuejschat/SpringbootVuejsChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dannyjae.springbootvuejschat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootVuejsChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
